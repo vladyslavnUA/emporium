d6948de423510903066312c0a05d809fedfe1637
vladyslavnUA <vladyslavn@yahoo.com>
2020-10-20 16:49:02 -0400

    implemented pagination on the home page

--- models/pet.js
+++ models/pet.js
@@ -3,6 +3,12 @@
 const mongoose = require('mongoose'),
         Schema = mongoose.Schema;
 
+const mongoosePaginate = require('mongoose-paginate');
+
+mongoosePaginate.paginate.options = {
+  limit: 3    // how many records on each page
+};
+
 const PetSchema = new Schema({
     name            : { type: String, required: true }
   , species         : { type: String }
@@ -16,4 +22,5 @@ const PetSchema = new Schema({
   timestamps: true
 });
 
+PetSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model('Pet', PetSchema);
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ module.exports = (app) => {
 
   /* GET home page. */
   app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
+    const page = req.query.page || 1
+
+    Pet.paginate({}, {page: page}).then((results) => {
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });    
     });
   });
 }
+
+// result.docs // the array of records on the current page
+// result.total // the total number of records
+// result.limit // the limit
+// result.page // the current page
+// result.pages // the total number of pages
