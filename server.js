3ebac48d18856c683fa008195adcaf7458de61a2
vladyslavnUA <vladyslavn@yahoo.com>
2020-11-11 18:56:44 -0500

    updated server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const methodOverride = require('method-override')
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/petes-pets');
+mongoose.connect('mongodb://localhost/petes-pets',  { useNewUrlParser: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
