aac04c4c1edbfcd158dd3a38f4e62e75ecde2358
vladyslavnUA <vladyslavn@yahoo.com>
2020-11-13 20:22:39 -0500

    Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -58,6 +58,19 @@ describe('Pets', ()  => {
       });
   });
 
+  // TEST API
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+      .get('/')
+      .set('content-type', 'application/json')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+    });
+  });
+
   // TEST CREATE 
   it('should create a SINGLE pet on /pets POST', (done) => {
     chai.request(server)
